2d85a211fbfb24410b4cfb0b080e00efb2ed82ff
Victor <Victor@Lenovo-Victor>
2016-01-21 15:19:16 -0800

    lab1

--- application/views/_template.php
+++ application/views/_template.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
-        <title>Simple Image Gallery</title>
+        <title>{pagetitle}</title>
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
         <link rel="stylesheet" type="text/css" media="all" href="css/reset.css" />
         <link rel="stylesheet" type="text/css" media="all" href="css/text.css" />
@@ -20,3 +20,15 @@
                     </ul>
                 </span>
             </div>
+            <div class="alone"></div>
+            <div id="content">
+                {content}
+            </div>
+            <div id="footer" class="span12">
+                Copyright &copy; 2014,  <a href="mailto:someone@somewhere.com">Me</a>.
+            </div>
+        </div>
+        <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script> 
+        <script type="text/javascript" src="js/lightbox.min.js"></script> 
+    </body>
+</html>
